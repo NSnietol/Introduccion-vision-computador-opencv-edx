b02d34f93973c45c711cb7fda7e1db2c135f1dc2
NSnietol <nilson-sn@outlook.com>
2018-03-02 17:51:46 -0500

    Update operaciones logicas

--- Semana 2/9. Operaciones Matemáticas/operacionesLogicas.py
+++ Semana 2/9. Operaciones Matemáticas/operacionesLogicas.py
@@ -14,45 +14,51 @@ import numpy as np
 
 file="LSI.jpg"
 file2="UC3M.jpg"
-img_1=cv2.imread(file)
-img_2=cv2.imread(file2)
+img_1=np.zeros((400,400,3)) # Se crea una 'imagen' de color negro completamente
+img_2=np.zeros((400,400,3))
+res1= cv2.bitwise_not(img_2)
+cv2.imshow('Not image 1',res1)
 
 if(img_1.data==None and img_2.data==None):
     print("Error")
 else:
     print("OK")
+    print(" ",(img_2))
     
+#La primera imagen se divide en 2, colocan una sección de color blanco
+for i in range(int(img_1.shape[0]/2)):
+    for j in range (img_1.shape[1]):
+        for RBG in range(3):
+            img_1.itemset((j,i,RBG),255)
 
-print("Propiedades\n")
-print("Imagne 1",img_1.shape)
-print("Imagen 2",img_2.shape)
-"""
-Necesito crear una imagen blanca y otra negra para cumplir con el ejemplo. 
-"""
-res=img_1+img_2
-#cv2.imshow('Suma de imagenes',res)
+#En la segunda imagen creamos un rectangulo blanco
+img_2[300:370:]=255
 
-res2=cv2.add(img_1,img_2)
 
-##cv2.imshow('Suma de imagenes con Cv2',res2)
 """
-AND 
+itwise Operations
+This includes bitwise AND, OR, NOT and XOR operations. They will be highly useful while extracting any part of the image 
 """
 
-res=cv2.bitwise_and(img_1,img_2)
+cv2.imshow("Imagen 1",img_1)
+cv2.imshow("Imagen 2",img_2)
+
+#AND
+res= cv2.bitwise_and(img_1,img_2)
 cv2.imshow('AND',res)
 
-
-res=cv2.bitwise_or(img_1,img_2)
+#OR
+res= cv2.bitwise_or(img_1,img_2)
 cv2.imshow('OR',res)
 
+#XOR
+res= cv2.bitwise_xor(img_1,img_2)
+cv2.imshow('XOR',res)
 
-res=cv2.bitwise_not(img_1)
-cv2.imshow('Not',res)
+#Not
+res= cv2.bitwise_not(img_2)
+cv2.imshow('Not image 1',res)
 
-
-res=cv2.bitwise_xor(img_1,img_2)
-cv2.imshow('XOR',res2)
 
 cv2.waitKey(0)
 
