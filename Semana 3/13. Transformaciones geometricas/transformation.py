7345873cac907e263737f866bd3687574fc67a8b
NSnietol <nilson-sn@outlook.com>
2018-03-12 15:25:31 -0500

    Update transformaciones geometricas

--- Semana 3/13. Transformaciones geometricas/transformation.py
+++ Semana 3/13. Transformaciones geometricas/transformation.py
@@ -15,11 +15,11 @@ purposes.
 """
 
 
-res =cv2.resize(img,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+res =cv2.resize(img,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
 #OR 
 
 height, width = img.shape[:2]
-res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_LINEAR)
 
 
 cv2.imshow('Scaling 50%',res)
